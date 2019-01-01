3092eb4479e31001549ae3c28d5256ebd422e66c
Stef <stef@roelofs-hengelo.nl>
2019-01-01 17:31:40 +0100

    * Added camera control

---
+++ Assets/Data/CameraController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Data
+{
+    class CameraController : MonoBehaviour
+    {
+        float mainSpeed = 100.0f; //regular speed
+        private float totalRun = 1.0f;
+
+        void Update()
+        {
+            //Keyboard commands
+            float f = 0.0f;
+            Vector3 p = GetBaseInput();
+            totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
+            p = p * mainSpeed;
+
+            p = p * Time.deltaTime;
+            Vector3 newPosition = transform.position;
+            transform.Translate(p);
+
+        }
+
+        private Vector3 GetBaseInput()
+        { //returns the basic values, if it's 0 than it's not active.
+            Vector3 p_Velocity = new Vector3();
+            if (Input.GetKey(KeyCode.W))
+            {
+                p_Velocity += new Vector3(0, 1, 0);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                p_Velocity += new Vector3(0, -1, 0);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                p_Velocity += new Vector3(-1, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                p_Velocity += new Vector3(1, 0, 0);
+            }
+            return p_Velocity;
+        }
+    }
+}
