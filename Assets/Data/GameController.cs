eb40093dd1f6cd59098ca8af9c3e32538130a53b
Stef <stef@roelofs-hengelo.nl>
2019-01-01 22:43:20 +0100

    * Added function to skip ahead by n days

--- Assets/Data/GameController.cs
+++ Assets/Data/GameController.cs
@@ -7,6 +7,7 @@ namespace Assets.Data
     public class GameController : MonoBehaviour
     {
         private ulong galacticTime;
+        private int galacticDay;
 
         public Galaxy Galaxy;
 
@@ -22,11 +23,46 @@ namespace Assets.Data
         {
         }
 
-        public void AdvanceTime(int numSeconds)
+
+        private int _prevSecond = 0;
+        private void FixedUpdate()
         {
-            galacticTime = galacticTime + (uint) numSeconds;
+            if (_prevSecond < (int)Time.time)
+            {
+                AdvanceTimeDay(10);
+                //AdvanceTime(1);
+                _prevSecond = (int)Time.time;
+            }
+        }
+
+        public void AdvanceTimeDay(int numDays)
+        {
+
+            ulong daysInSeconds = (24 * 60 * 60) * (uint)numDays;
+
+            galacticTime = galacticTime + (uint)daysInSeconds;
 
             Galaxy.Update(galacticTime);
+            galacticDay += numDays;
+
+            if (galacticDay >= 30)
+            {
+                // Dirty fix to ensure the current day is no more than 30, should do something with months and years ad stuff...
+                // Don't do this to the seconds just yet since they determine the current place of the orbital
+                galacticDay = 0;
+            }
+
+            //Debug.Log("Current day: " + galacticDay);
+            //Debug.Log("Current galacticTime: " + galacticTime);
         }
+        public void AdvanceTime(int numSeconds)
+        {
+            galacticTime = galacticTime + (uint)numSeconds;
+
+            Galaxy.Update(galacticTime);
+            //Debug.Log("Current day: " + galacticDay);
+            //Debug.Log("Current galacticTime: " + galacticTime);
+        }
+
     }
 }
