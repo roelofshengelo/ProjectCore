9f63a31cefce7b5d47c69c662d8e2520077b8a47
Stef <stef@roelofs-hengelo.nl>
2019-01-08 19:58:57 +0100

    * Removed Parralax background * Fixed camera movement with wasd

--- Assets/Data/CameraController.cs
+++ Assets/Data/CameraController.cs
@@ -1,106 +1,40 @@
-﻿using System;
-using UnityEngine;
-using System.Collections;
-using UnityEditor;
+﻿using UnityEngine;
 
-namespace Assets.Data
+public class CameraController : MonoBehaviour
 {
-    class CameraController : MonoBehaviour
+    float mainSpeed = 100.0f; //regular speed
+    private float totalRun = 1.0f;
+
+    void Update()
     {
-        // Camera movement
-        float mainSpeed = 100.0f; //regular speed
-        private float totalRun = 1.0f;
+        //Keyboard commands
+        Vector3 p = GetBaseInput();
+        totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
+        p = p * mainSpeed;
+        p = p * Time.deltaTime;
 
-        //Camera background
+        transform.Translate(p);
+    }
 
-        // ReSharper disable once MemberCanBePrivate.Global
-        // ReSharper disable once FieldCanBeMadeReadOnly.Global
-        public Transform[] Backgrounds;
-
-        // ReSharper disable once MemberCanBePrivate.Global
-        public float[] ParallaxScales;
-
-        // ReSharper disable once MemberCanBePrivate.Global
-        public float Smoothing = 0f;
-
-        //private Transform cam;
-
-        private Vector3 previousCamPos;
-
-        public CameraController(Transform[] backgrounds)
+    private Vector3 GetBaseInput()
+    { //returns the basic values, if it's 0 than it's not active.
+        Vector3 pVelocity = new Vector3();
+        if (Input.GetKey(KeyCode.W))
         {
-            Backgrounds = backgrounds;
+            pVelocity += new Vector3(0, 1, 0);
         }
-
-        void Start()
+        if (Input.GetKey(KeyCode.S))
         {
-            if (Backgrounds is null) throw new ArgumentNullException("backgrounds of CameraController must be set!");
-            if (ParallaxScales is null) throw new ArgumentNullException("parallaxScales of CameraController must be set!");
-            //if (Smoothing is 0f) throw new ArgumentNullException("parallaxScales of CameraController must be set!");
-
-            previousCamPos = transform.position;
-            ParallaxScales = new float[Backgrounds.Length];
-
-            for (int i = 0; i < Backgrounds.Length; i++)
-            {
-                ParallaxScales[i] = Backgrounds[i].position.z * -1;
-                //Debug.Log(parallaxScales[i]);
-
-            }
-
+            pVelocity += new Vector3(0, -1, 0);
         }
-
-        void Update()
+        if (Input.GetKey(KeyCode.A))
         {
-            //Mouse commands
-            transform.Translate(new Vector3(0, 0, Input.mouseScrollDelta.y));
-            for (int i = 0; i < Backgrounds.Length; i++)
-            {
-                var parallaxX = (previousCamPos.x - transform.position.x) * ParallaxScales[i];
-                var parallaxY = (previousCamPos.y - transform.position.y) * ParallaxScales[i];
-                //var parallaxZ = (previousCamPos.z - transform.position.z);
-
-                var backgroundTargetPosX = Backgrounds[i].position.x + parallaxX;
-                var backgroundTargetPosy = Backgrounds[i].position.y + parallaxY;
-                //var backgroundTargetPosZ = transform.position.z; //backgrounds[i].position.z - parallaxZ;
-
-                var backgroundTargetPos = new Vector3(backgroundTargetPosX, backgroundTargetPosy, Backgrounds[i].position.z);
-
-                Backgrounds[i].position = Vector3.Lerp(Backgrounds[i].position, backgroundTargetPos, Smoothing * Time.deltaTime);
-            }
-            previousCamPos = transform.position;
-
-            //Keyboard commands
-            float f = 0.0f;
-            Vector3 p = GetBaseInput();
-            totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
-            p = p * mainSpeed;
-
-            p = p * Time.deltaTime;
-            transform.Translate(p);
+            pVelocity += new Vector3(-1, 0, 0);
         }
-
-
-        private Vector3 GetBaseInput()
-        { //returns the basic values, if it's 0 than it's not active.
-            Vector3 pVelocity = new Vector3();
-            if (Input.GetKey(KeyCode.W))
-            {
-                pVelocity += new Vector3(0, 1, 0);
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                pVelocity += new Vector3(0, -1, 0);
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                pVelocity += new Vector3(-1, 0, 0);
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                pVelocity += new Vector3(1, 0, 0);
-            }
-            return pVelocity;
+        if (Input.GetKey(KeyCode.D))
+        {
+            pVelocity += new Vector3(1, 0, 0);
         }
+        return pVelocity;
     }
 }
