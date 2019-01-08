74c79321b8b068ce6775043a8105fb650199520a
Stef <stef@roelofs-hengelo.nl>
2019-01-08 20:00:32 +0100

    * Added namespace back to GameController

--- Assets/Data/CameraController.cs
+++ Assets/Data/CameraController.cs
@@ -1,40 +1,43 @@
 ﻿using UnityEngine;
 
-public class CameraController : MonoBehaviour
+namespace Assets.Data
 {
-    float mainSpeed = 100.0f; //regular speed
-    private float totalRun = 1.0f;
-
-    void Update()
+    public class CameraController : MonoBehaviour
     {
-        //Keyboard commands
-        Vector3 p = GetBaseInput();
-        totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
-        p = p * mainSpeed;
-        p = p * Time.deltaTime;
+        float mainSpeed = 100.0f; //regular speed
+        private float totalRun = 1.0f;
 
-        transform.Translate(p);
-    }
+        void Update()
+        {
+            //Keyboard commands
+            Vector3 p = GetBaseInput();
+            totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
+            p = p * mainSpeed;
+            p = p * Time.deltaTime;
 
-    private Vector3 GetBaseInput()
-    { //returns the basic values, if it's 0 than it's not active.
-        Vector3 pVelocity = new Vector3();
-        if (Input.GetKey(KeyCode.W))
-        {
-            pVelocity += new Vector3(0, 1, 0);
+            transform.Translate(p);
         }
-        if (Input.GetKey(KeyCode.S))
-        {
-            pVelocity += new Vector3(0, -1, 0);
+
+        private Vector3 GetBaseInput()
+        { //returns the basic values, if it's 0 than it's not active.
+            Vector3 pVelocity = new Vector3();
+            if (Input.GetKey(KeyCode.W))
+            {
+                pVelocity += new Vector3(0, 1, 0);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                pVelocity += new Vector3(0, -1, 0);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                pVelocity += new Vector3(-1, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                pVelocity += new Vector3(1, 0, 0);
+            }
+            return pVelocity;
         }
-        if (Input.GetKey(KeyCode.A))
-        {
-            pVelocity += new Vector3(-1, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            pVelocity += new Vector3(1, 0, 0);
-        }
-        return pVelocity;
     }
 }
